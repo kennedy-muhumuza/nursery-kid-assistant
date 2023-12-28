ca15c6826966c9e1aab156721b01b1e41b254273
Johnkennedy <muhumuzakennedy17@gmail.com>
2023-12-28 16:36:00 +0300

    gallery drop down

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -56,8 +56,8 @@ const Chat = () => {
 
       setTranscript(event.results[last][0].transcript);
 
-      const response = await fetch("http://127.0.0.1:8000/botly", {
-        // const response = await fetch("https://botly-backend.onrender.com", {
+      // const response = await fetch("http://127.0.0.1:8000/botly", {
+      const response = await fetch("https://botly-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
